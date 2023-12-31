722d8fd93a16fb30a92a1d24959d5327bc73994a
Thomas Masha <thomas8483828180@gmail.com>
2023-12-31 03:11:19 -0600

    go
    github is mean to me

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/pulse/pulsar
+# Install script for directory: C:/Users/plut0/OneDrive/Documents/GitHub/pulsar
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Pulsar")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "")
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -27,26 +27,16 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-# Set default install directory permissions.
-if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
-endif()
-
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/pulse/pulsar/build/external/glfw/cmake_install.cmake")
-  include("/home/pulse/pulsar/build/external/glad/cmake_install.cmake")
-  include("/home/pulse/pulsar/build/external/glm/cmake_install.cmake")
+  include("C:/Users/plut0/OneDrive/Documents/GitHub/pulsar/build/external/glfw/cmake_install.cmake")
+  include("C:/Users/plut0/OneDrive/Documents/GitHub/pulsar/build/external/glad/cmake_install.cmake")
+  include("C:/Users/plut0/OneDrive/Documents/GitHub/pulsar/build/external/glm/cmake_install.cmake")
 
 endif()
 
@@ -58,5 +48,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/pulse/pulsar/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/plut0/OneDrive/Documents/GitHub/pulsar/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
